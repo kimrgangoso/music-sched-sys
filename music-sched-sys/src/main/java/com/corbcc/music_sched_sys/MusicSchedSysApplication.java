513df8d5c5660fc2ab3d153eef932951fb3e1cf2
kimrgangoso <kimrgangoso@gmail.com>
2024-07-16 16:27:00 +0800

    Initial Commit

---
+++ music-sched-sys/src/main/java/com/corbcc/music_sched_sys/MusicSchedSysApplication.java
@@ -0,0 +1,13 @@
+package com.corbcc.music_sched_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicSchedSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicSchedSysApplication.class, args);
+	}
+
+}
---
+++ music-sched-sys/src/test/java/com/corbcc/music_sched_sys/MusicSchedSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corbcc.music_sched_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicSchedSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
